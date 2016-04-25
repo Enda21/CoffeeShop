99bdf9f8d8ca1b1f4efe74e3b360144f50477308
enda <pbrody111@gmail.com>
2016-04-25 12:40:04 +0100

    11th commit

--- composer.js
+++
@@ -1,9 +0,0 @@
-{
-    "require-dev": {
-		"munkie/phpunit-teamcity-testlistener": "~1.2"
-		"phpunit/phpunit-selenium": ">=1.2"
-    },
-    "require": {
-        "phpunit/phpunit": "^5.3"
-    }
-}
